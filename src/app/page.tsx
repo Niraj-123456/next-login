727c06ccafde709668455498a51ede606df8299d
nirajlama <nlama@hamropatro.com>
2024-07-03 23:04:09 +0545

    (fix) wrapped login route with suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Login from "@next/components/login/Login";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <Login />
-    </div>
+    </Suspense>
   );
 }
